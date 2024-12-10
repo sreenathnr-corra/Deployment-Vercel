3576e4d6090ab9bc437cba6688bf0f0887c14aa4
sreenathnr-corra <sreenathnr@corra.com>
2024-12-10 12:43:02 +0530

    Prisma changes

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -22,36 +22,11 @@ export default function App() {
   return (
     <div className={styles.index}>
       <div className={styles.content}>
-        <h1 className={styles.heading}>A short heading about [your app]</h1>
+        <h1 className={styles.heading}>Vercel Deployment</h1>
         <p className={styles.text}>
-          A tagline about [your app] that describes your value proposition.
+          Test deployment for vercel
         </p>
-        {showForm && (
-          <Form className={styles.form} method="post" action="/auth/login">
-            <label className={styles.label}>
-              <span>Shop domain</span>
-              <input className={styles.input} type="text" name="shop" />
-              <span>e.g: my-shop-domain.myshopify.com</span>
-            </label>
-            <button className={styles.button} type="submit">
-              Log in
-            </button>
-          </Form>
-        )}
-        <ul className={styles.list}>
-          <li>
-            <strong>Product feature</strong>. Some detail about your feature and
-            its benefit to your customer.
-          </li>
-          <li>
-            <strong>Product feature</strong>. Some detail about your feature and
-            its benefit to your customer.
-          </li>
-          <li>
-            <strong>Product feature</strong>. Some detail about your feature and
-            its benefit to your customer.
-          </li>
-        </ul>
+        
       </div>
     </div>
   );
